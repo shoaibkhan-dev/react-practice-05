4fc0abe4036ff17c89efaed3a4aaa551d73268e6
Shoaib khan <shoaibmangal@yahoo.com>
2025-11-02 22:52:07 +0500

    useref hook use for input focus

--- react-web/src/components/InputFocus.jsx
+++ react-web/src/components/InputFocus.jsx
@@ -1,14 +1,18 @@
 import { useRef } from "react";
 
 function InputFocus() {
- 
+  const inputRef = useRef(null);
+
+  const handleClick = () => {
+    inputRef.current.focus(); // direct DOM element access
   };
 
   return (
     <div>
-     
+      <input ref={inputRef} type="text" placeholder="Type here..." />
+      <button onClick={handleClick}>Focus Input</button>
     </div>
   );
-
+}
 
 export default InputFocus;
