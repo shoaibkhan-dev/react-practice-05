0421c359dd203e766f8799e7b40f0e88f9847bea
Shoaib khan <shoaibmangal@yahoo.com>
2025-11-08 23:52:38 +0500

    add div for new work

--- react-web/src/components/Form/InputFocus.jsx
+++ react-web/src/components/Form/InputFocus.jsx
@@ -28,49 +28,61 @@
 
 // export default InputFocus;
 
+// import React from 'react'
+// import { useState } from 'react'
+// import './InputFocus.css'
+
+// const InputFocus = () => {
+//     const [title, setTitle] = useState('')
+//     const [detail, setDetail] = useState('')
+//     const [task, setTask] = useState('')
+//     const handleSubmit = (e) =>{
+//        e.preventDefault()
+//        console.log(title, detail)
+//        setTitle('')
+//        setDetail('')
+//     }
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         handleSubmit(e)
+//       }}>
+//         <input type="text" 
+//         placeholder='enter note' 
+//         value={title}
+//         onChange={(e)=>{
+//           setTitle(e.target.value)
+//         }}
+//         />
+//         <br />
+//         <textarea type="text"
+//          placeholder='write detail'
+//         className='inp'
+//          value={detail}
+//          onChange={(e)=>{
+//           setDetail(e.target.value)
+//          }}
+//           />
+//           <br />
+//         <button className='btn'>Add Note</button>
+//       </form>
+//       <div className='output'>
+//         <h1>This is Output</h1>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default InputFocus;
+
 import React from 'react'
-import { useState } from 'react'
-import './InputFocus.css'
 
 const InputFocus = () => {
-    const [title, setTitle] = useState('')
-    const [detail, setDetail] = useState('')
-    const [task, setTask] = useState('')
-    const handleSubmit = (e) =>{
-       e.preventDefault()
-       console.log(title, detail)
-       setTitle('')
-       setDetail('')
-    }
   return (
     <div>
-      <form onSubmit={(e)=>{
-        handleSubmit(e)
-      }}>
-        <input type="text" 
-        placeholder='enter note' 
-        value={title}
-        onChange={(e)=>{
-          setTitle(e.target.value)
-        }}
-        />
-        <br />
-        <textarea type="text"
-         placeholder='write detail'
-        className='inp'
-         value={detail}
-         onChange={(e)=>{
-          setDetail(e.target.value)
-         }}
-          />
-          <br />
-        <button className='btn'>Add Note</button>
-      </form>
-      <div className='output'>
-        <h1>This is Output</h1>
-      </div>
+      
     </div>
   )
 }
 
-export default InputFocus;
+export default InputFocus
