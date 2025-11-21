cc5994f8e4bc81c29b948ca9061b24dd906afc98
Shoaib khan <shoaibmangal@yahoo.com>
2025-11-21 23:55:43 +0500

    button

--- react-web/src/components/ExploreMenu/ExploreMenu.jsx
+++ react-web/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,6 +5,7 @@ const ExploreMenu = () => {
     <div>
       <h1>This is explore Menu</h1>
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ex.</h2>
+      <button>click me</button>
     </div>
   )
 }
