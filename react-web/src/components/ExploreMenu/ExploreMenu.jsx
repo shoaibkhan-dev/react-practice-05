fb012249b14d57484eb74f0044882cbc0b6970fd
Shoaib khan <shoaibmangal@yahoo.com>
2025-11-21 23:54:57 +0500

    h2 heading in explore menu jsx

--- react-web/src/components/ExploreMenu/ExploreMenu.jsx
+++ react-web/src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,6 +4,7 @@ const ExploreMenu = () => {
   return (
     <div>
       <h1>This is explore Menu</h1>
+      <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ex.</h2>
     </div>
   )
 }
