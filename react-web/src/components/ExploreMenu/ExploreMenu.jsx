aa477a0a488f870d3c8f4f4bf042b80a7c510227
Shoaib khan <shoaibmangal@yahoo.com>
2025-11-22 23:56:23 +0500

    form added with inputs

--- react-web/src/components/ExploreMenu/ExploreMenu.jsx
+++ react-web/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,6 +5,10 @@ const ExploreMenu = () => {
     <div>
       <h1>This is explore Menu</h1>
       <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, ex.</h2>
+      <form>
+        <input type="text" placeholder='enter name' />
+        <input type="text" placeholder='enter email' />
+      </form>
       <button>click me</button>
     </div>
   )
